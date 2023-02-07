454459a746b4dcc457978baec5b2743179f811ef
Andrei Yankavets <andreyyankovez@gmail.com>
2023-02-07 14:25:15 +0300

    Transfer project to Spring Boot, Fix html hardcode

---
+++ src/main/java/by/yankavets/library/SpringMiniLibraryApplication.java
@@ -0,0 +1,13 @@
+package by.yankavets.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMiniLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMiniLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/yankavets/SpringMiniLibrary/SpringMiniLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package by.yankavets.SpringMiniLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMiniLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
